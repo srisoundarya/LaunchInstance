dc62e835cec64a1873a342bf4432e4ad9c06f043
13143 <13143@Subbu.lan>
2024-03-06 19:29:02 -0600

    Linking Eclipse to Github

---
+++ src/launchpackage/launchclass.java
@@ -0,0 +1,12 @@
+package launchpackage;
+
+public class launchclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Created - Instance, Repository, New Item in Jenkins ");
+		System.out.println("linking Eclipse to Github");
+		System.out.println("checking status");
+	}
+
+}
